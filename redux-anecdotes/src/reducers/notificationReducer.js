54c2b371f9619357c52acff2f866da83d02bd084
arskdj <68353557+arskdj@users.noreply.github.com>
2020-09-01 20:32:07 +0300

    6.10

---
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,21 @@
+const message = 'default notification message'
+
+const notificationReducer = (state=message, action) => {
+    switch (action.type){
+        case 'SET_NOTIFICATION':
+            return state.data.message
+        default:
+            return state
+    } 
+}
+
+export const setNotification = (message) => {
+    return {
+        type : 'SET_NOTIFICATION',
+        data : {
+            message
+        }
+    } 
+}
+
+export default notificationReducer
